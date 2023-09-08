85d7881003728238bb333d29bb6f2bf8d85bcb7e
ashishramesh9 <ashishramesh9@gmail.com>
2023-09-08 16:53:41 +0530

    Shell script

---
+++ first.sh
@@ -0,0 +1,21 @@
+######################################
+#Author: Ashish
+#Date: 8 sept
+#####################################
+#
+#This script is to show the utilization of the rezsources
+#
+#####################################
+
+echo "Disk partion"
+df -h
+
+
+echo "Free space"
+free -h
+
+
+echo "Cpu running"
+ps -ef
+
+
